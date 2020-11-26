f5dcb0937a4d5c414f5f97de6fb263d71e055e48
Duy Tran Khanh <40482367+khanhduytran0@users.noreply.github.com>
2020-11-26 11:51:20 +0700

    Update prefetch.inline.hpp

--- hotspot/src/share/vm/runtime/prefetch.inline.hpp
+++ hotspot/src/share/vm/runtime/prefetch.inline.hpp
@@ -40,6 +40,9 @@
 #ifdef TARGET_OS_ARCH_linux_arm
 # include "prefetch_linux_arm.inline.hpp"
 #endif
+#ifdef TARGET_OS_ARCH_linux_aarch32
+# include "prefetch_linux_aarch32.inline.hpp"
+#endif
 #ifdef TARGET_OS_ARCH_linux_aarch64
 # include "prefetch_linux_aarch64.inline.hpp"
 #endif
