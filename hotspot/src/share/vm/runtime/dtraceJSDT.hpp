c3eb8e928743fd59a680a25c27f048cb3b789e9d
Duy Tran Khanh <40482367+khanhduytran0@users.noreply.github.com>
2020-11-26 12:34:35 +0700

    Update dtraceJSDT.hpp

--- hotspot/src/share/vm/runtime/dtraceJSDT.hpp
+++ hotspot/src/share/vm/runtime/dtraceJSDT.hpp
@@ -29,6 +29,9 @@
 #ifdef TARGET_ARCH_x86
 # include "nativeInst_x86.hpp"
 #endif
+#ifdef TARGET_ARCH_aarch32
+# include "nativeInst_aarch32.hpp"
+#endif
 #ifdef TARGET_ARCH_aarch64
 # include "nativeInst_aarch64.hpp"
 #endif
