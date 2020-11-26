afc1c2215a154fcb9c41d49428705bbf00bb3d07
Duy Tran Khanh <40482367+khanhduytran0@users.noreply.github.com>
2020-11-26 19:41:32 +0700

    Update codeBuffer.hpp

--- hotspot/src/share/vm/asm/codeBuffer.hpp
+++ hotspot/src/share/vm/asm/codeBuffer.hpp
@@ -620,6 +620,9 @@ class CodeBuffer: public StackObj {
 #ifdef TARGET_ARCH_x86
 # include "codeBuffer_x86.hpp"
 #endif
+#ifdef TARGET_ARCH_aarch32
+# include "codeBuffer_aarch32.hpp"
+#endif
 #ifdef TARGET_ARCH_aarch64
 # include "codeBuffer_aarch64.hpp"
 #endif
