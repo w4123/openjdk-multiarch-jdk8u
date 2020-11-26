77456b8826cbc7dd31893862a43072c1b8b0fc7f
Duy Tran Khanh <40482367+khanhduytran0@users.noreply.github.com>
2020-11-26 11:52:34 +0700

    Add aarch32

--- hotspot/src/share/vm/runtime/os.hpp
+++ hotspot/src/share/vm/runtime/os.hpp
@@ -858,6 +858,9 @@ class os: AllStatic {
 #ifdef TARGET_OS_ARCH_linux_x86
 # include "os_linux_x86.hpp"
 #endif
+#ifdef TARGET_OS_ARCH_linux_aarch32
+# include "os_linux_aarch32.hpp"
+#endif
 #ifdef TARGET_OS_ARCH_linux_aarch64
 # include "os_linux_aarch64.hpp"
 #endif
