7b6b73bb23b0178ff0112bd7585994a47b64a8ae
Duy Tran Khanh <40482367+khanhduytran0@users.noreply.github.com>
2020-11-26 17:43:47 +0700

    Update disassembler.cpp

--- hotspot/src/share/vm/compiler/disassembler.cpp
+++ hotspot/src/share/vm/compiler/disassembler.cpp
@@ -35,6 +35,9 @@
 #ifdef TARGET_ARCH_x86
 # include "depChecker_x86.hpp"
 #endif
+#ifdef TARGET_ARCH_aarch32
+# include "depChecker_aarch32.hpp"
+#endif
 #ifdef TARGET_ARCH_aarch64
 # include "depChecker_aarch64.hpp"
 #endif
