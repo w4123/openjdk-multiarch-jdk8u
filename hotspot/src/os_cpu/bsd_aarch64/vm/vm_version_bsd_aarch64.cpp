6d2e1382cff2d042a8e2725d8db83cd63c949c5b
Duy Tran Khanh <40482367+khanhduytran0@users.noreply.github.com>
2021-05-16 14:46:06 +0700

    Move

--- hotspot/src/os_cpu/bsd_aarch64/vm/vm_version_bsd_aarch64.cpp
+++ hotspot/src/os_cpu/bsd_aarch64/vm/vm_version_bsd_aarch64.cpp
@@ -1,7 +1,5 @@
 /*
- * Copyright (c) 2006, 2021, Oracle and/or its affiliates. All rights reserved.
- * Copyright (c) 2014, 2019, Red Hat Inc. All rights reserved.
- * Copyright (c) 2021, Azul Systems, Inc. All rights reserved.
+ * Copyright (c) 2006, 2010, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -25,62 +23,5 @@
  */
 
 #include "precompiled.hpp"
-#include "runtime/java.hpp"
 #include "runtime/os.hpp"
 #include "vm_version_aarch64.hpp"
-
-#include <sys/sysctl.h>
-
-static bool cpu_has(const char* optional) {
-  uint32_t val;
-  size_t len = sizeof(val);
-  if (sysctlbyname(optional, &val, &len, NULL, 0)) {
-    return false;
-  }
-  return val;
-}
-
-unsigned long VM_Version::os_get_processor_features() {
-  unsigned long auxv = 0;
-  size_t sysctllen;
-
-  // hw.optional.floatingpoint always returns 1, see
-  // https://github.com/apple/darwin-xnu/blob/master/bsd/kern/kern_mib.c#L416.
-  // ID_AA64PFR0_EL1 describes AdvSIMD always equals to FP field.
-  assert(cpu_has("hw.optional.floatingpoint"), "should be");
-  assert(cpu_has("hw.optional.neon"), "should be");
-  auxv = CPU_FP | CPU_ASIMD;
-
-  // Only few features are available via sysctl, see line 614
-  // https://opensource.apple.com/source/xnu/xnu-6153.141.1/bsd/kern/kern_mib.c.auto.html
-  if (cpu_has("hw.optional.armv8_crc32"))     auxv |= CPU_CRC32;
-  if (cpu_has("hw.optional.armv8_1_atomics")) auxv |= CPU_LSE;
-
-  int cache_line_size;
-  int hw_conf_cache_line[] = { CTL_HW, HW_CACHELINE };
-  sysctllen = sizeof(cache_line_size);
-  if (sysctl(hw_conf_cache_line, 2, &cache_line_size, &sysctllen, NULL, 0)) {
-    cache_line_size = 16;
-  }
-  _icache_line_size = 16; // minimal line lenght CCSIDR_EL1 can hold
-  _dcache_line_size = cache_line_size;
-
-  uint64_t dczid_el0;
-  __asm__ (
-    "mrs %0, DCZID_EL0\n"
-    : "=r"(dczid_el0)
-  );
-  if (!(dczid_el0 & 0x10)) {
-    _zva_length = 4 << (dczid_el0 & 0xf);
-  }
-
-  int family;
-  sysctllen = sizeof(family);
-  if (sysctlbyname("hw.cpufamily", &family, &sysctllen, NULL, 0)) {
-    family = 0;
-  }
-  _model = family;
-  _cpu = CPU_APPLE;
-
-  return auxv;
-}
