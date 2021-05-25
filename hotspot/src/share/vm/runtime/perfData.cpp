6fc99cde775791e6e7a4f552631cd0f1b7cef689
Duy Tran Khanh <40482367+khanhduytran0@users.noreply.github.com>
2021-05-25 07:05:32 +0700

    Workaround "PerfData::~PerfData" without mem leak?
    See the previous commit, related to this

--- hotspot/src/share/vm/runtime/perfData.cpp
+++ hotspot/src/share/vm/runtime/perfData.cpp
@@ -112,13 +112,17 @@ PerfData::PerfData(CounterNS ns, const char* name, Units u, Variability v)
 }
 
 PerfData::~PerfData() {
-  // iOS port: FIXME crash as breakpoint
 #ifndef __APPLE__
   if (_name != NULL) {
     FREE_C_HEAP_ARRAY(char, _name, mtInternal);
   }
   if (is_on_c_heap()) {
     FREE_C_HEAP_ARRAY(PerfDataEntry, _pdep, mtInternal);
+  }
+#else
+  FREE_C_HEAP_ARRAY(char, _name);
+  if (is_on_c_heap()) {
+    FREE_C_HEAP_ARRAY(PerfDataEntry, _pdep);
   }
 #endif
 }
