19d2c1ac64f65ddb4fead40a6f1edd6b145d0929
Duy Tran Khanh <40482367+khanhduytran0@users.noreply.github.com>
2021-05-25 10:50:46 +0700

    [iOS] Let the memory leaks because EXC_BREAKPOINT
    On iOS port, idk why, but `PerfData::~PerfData()` causes a breakpoint crash? Anyways, let the JVM exit with the leaked memory, so it will be cleaned.

--- hotspot/src/share/vm/runtime/perfMemory.cpp
+++ hotspot/src/share/vm/runtime/perfMemory.cpp
@@ -32,7 +32,6 @@
 #include "runtime/os.hpp"
 #include "runtime/perfData.hpp"
 #include "runtime/perfMemory.hpp"
-#include "runtime/safepoint.hpp"
 #include "runtime/statSampler.hpp"
 #include "utilities/globalDefinitions.hpp"
 
@@ -65,17 +64,16 @@ void perfMemory_exit() {
   if (!UsePerfData) return;
   if (!PerfMemory::is_initialized()) return;
 
-  // Only destroy PerfData objects if we're at a safepoint and the
-  // StatSampler is not active. Otherwise, we risk removing PerfData
-  // objects that are currently being used by running JavaThreads
-  // or the StatSampler. This method is invoked while we are not at
-  // a safepoint during a VM abort so leaving the PerfData objects
-  // around may also help diagnose the failure. In rare cases,
-  // PerfData objects are used in parallel with a safepoint. See
-  // the work around in PerfDataManager::destroy().
+  // if the StatSampler is active, then we don't want to remove
+  // resources it may be dependent on. Typically, the StatSampler
+  // is disengaged from the watcher thread when this method is called,
+  // but it is not disengaged if this method is invoked during a
+  // VM abort.
   //
-  if (SafepointSynchronize::is_at_safepoint() && !StatSampler::is_active())
+#ifndef __APPLE__
+  if (!StatSampler::is_active())
     PerfDataManager::destroy();
+#endif
 
   // remove the persistent external resources, if any. this method
   // does not unmap or invalidate any virtual memory allocated during
